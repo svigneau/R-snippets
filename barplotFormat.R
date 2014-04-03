ed2f1fc38ac7c1fa9df75058e2ff1092d3216603
Sebastien Vigneau <sebastien.vigneau@gmail.com>
2014-04-03 13:40:08 -0400

    Fixed bug

--- barplotFormat.R
+++ barplotFormat.R
@@ -10,7 +10,7 @@ barplot_format <- function (data, xlabel, ylabel) {
   summary <- data.frame(xlabel, ylabel, data)
   summary_cast <- cast(summary, ylabel ~ xlabel, value='data')
   summary_mat <- as.matrix(summary_cast[, 2:3])
-  dimnames(summary_mat) <- list(xlabel, ylabel)
+  dimnames(summary_mat) <- list(unique(xlabel), unique(ylabel))
   summary_prop <- apply(summary_mat, 2, function (x) { x / sum(x) })
   return(summary_prop)
 }
@@ -21,8 +21,8 @@ barplot_format <- function (data, xlabel, ylabel) {
 #data <- c(val1, val2, val3, val4)
 
 # Labels for x and y axis
-#xlabel <- c(label1, label2)
-#ylabel <- c(labela, labelb)
+#xlabel <- c(label1, label1, label2, label2)
+#ylabel <- c(labela, labelb, labela, labelb)
 
 # Format data
 #barplotdata <- barplot_format(data, xlabel, ylabel)
